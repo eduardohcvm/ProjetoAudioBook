70ef1c628fe135fb5ecdfab1bdedf004f6a974ab
Eduardo Henrique <eduardohcvm@gmail.com>
2024-01-29 21:26:25 -0300

    thurd

--- AudioBook.js
+++ AudioBook.js
@@ -1,2 +1,27 @@
-let a;
-var b;
+const botaoPLayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+let numerosCapitulos = 10;
+let taTocando = 0;
+
+function tocarFaixa() {
+    audioCapitulo.play();
+}
+
+function pausarFaixa(){
+    audioCapitulo.pause();
+}
+
+function tocarOuPausar() {
+    if (taTocando === 0) {
+        tocarFaixa();
+        taTocando = 1;
+    }    
+    else{
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+
+
+botaoPLayPause.addEventListener('click', tocarOuPausar);
