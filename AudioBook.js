0b7e6d031d6a2cbc21fc150f4df8e88efb1fda8d
Eduardo Henrique <eduardohcvm@gmail.com>
2024-01-29 22:22:54 -0300

    projeto finalizado

--- AudioBook.js
+++ AudioBook.js
@@ -1,15 +1,23 @@
 const botaoPLayPause = document.getElementById('play-pause');
 const audioCapitulo = document.getElementById('audio-capitulo');
+const proximaFaixa = document.getElementById('proximo');
+const faixaAnterior = document.getElementById('anterior');
+const capAtual = document.getElementById('capitulo')
 
 let numerosCapitulos = 10;
+let capitulo = 1;
 let taTocando = 0;
 
 function tocarFaixa() {
     audioCapitulo.play();
+    botaoPLayPause.classList.remove('bi-play-circle-fill');
+    botaoPLayPause.classList.add('bi-pause-circle-fill');
 }
 
 function pausarFaixa(){
     audioCapitulo.pause();
+    botaoPLayPause.classList.remove('bi-pause-circle-fill');
+    botaoPLayPause.classList.add('bi-play-circle-fill');
 }
 
 function tocarOuPausar() {
@@ -23,5 +31,38 @@ function tocarOuPausar() {
     }
 }
 
+function proximaF() {
+    if (capitulo === numerosCapitulos) {
+        capitulo = 1;
+    } else {
+        capitulo = capitulo + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capitulo + '.mp3';
+    tocarFaixa();
+    capAtual.innerText = "Capítulo " + capitulo;
+    taTocando = 1;
+}
+
+function anteriorF() {
+    if (capitulo === 1) {
+        capitulo = numerosCapitulos;
+    } else {
+        capitulo = capitulo - 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capitulo + '.mp3';
+    tocarFaixa();
+    capAtual.innerText = "Capítulo " + capitulo;
+    taTocando = 1;
+}
+
+
+function trocarNomeFaixa() {
+    
+}
+
 
 botaoPLayPause.addEventListener('click', tocarOuPausar);
+proximaFaixa.addEventListener('click', proximaF);
+faixaAnterior.addEventListener('click', anteriorF);
